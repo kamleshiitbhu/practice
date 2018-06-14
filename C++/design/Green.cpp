74327250c5f75ef1a627636e3413cf1019867538
Kamlesh Solanki <kamleshkumar356@gmail.com>
2018-06-14 22:50:49 +0530

    change design directory

---
+++ C++/design/Green.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "Green.h"
+
+void Green::display(void){
+  cout<<"This is Green Color"<<endl;
+
+}
---
+++ C++/design/Green.h
@@ -0,0 +1,11 @@
+#ifndef __GREEN_H__
+#define __GREEN_H__
+
+#include "Color.h"
+
+class Green : public Color{
+public:
+  void  display(void);
+};
+
+#endif //__GREEN_H__
